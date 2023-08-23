7b16c7b2c8d244cd7e5cb433436c6eb4b5715c8d
Iury <iury.silvafim@gmail.com>
2023-08-23 06:51:12 -0300

    Cnpj

---
+++ Serialização e Desserialização/Program.cs
@@ -0,0 +1,16 @@
+﻿using Validacao;
+
+namespace Serialização_e_Desserialização
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Write("Digite o seu CNPJ: ");
+            string cnpj = Console.ReadLine();
+            string cn = ValidaCNPJ.IsCnpj(cnpj).ToString();
+            Console.WriteLine(cn);
+        }
+    }
+}
